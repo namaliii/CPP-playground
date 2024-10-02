4adee8d89f13447afae1aaab03f24bf8f2503a2a
Alicja <alicjanamieta@gmail.com>
2024-10-02 20:58:24 +0200

    adding contacts and searching for them

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 19:21:23 by anamieta          #+#    #+#             */
-/*   Updated: 2024/10/02 20:57:30 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/10/02 20:58:04 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,4 +59,51 @@ std::string PhoneBook::inputField(const std::string& fieldName) {
 		std::getline(std::cin, input);
 	}
 	return input;
+}
+
+void PhoneBook::addContact() {
+	int index = getContactIndex();
+	std::cout << "Adding new contact at index: " << index + 1 << std::endl;
+	contacts[index].setFirstName(inputField("first name"));
+	std::cout << "First Name: " << contacts[index].getFirstName() << std::endl; // Debugging
+	contacts[index].setLastName(inputField("last name"));
+	contacts[index].setNickname(inputField("nickname"));
+	std::string phoneNumber;
+	do {
+		phoneNumber = inputField("phone number");
+		if (!validatePhoneNumber(phoneNumber))
+			std::cout << "Invalid phone number. Please enter a valid phone number." << std::endl;
+	} while (!validatePhoneNumber(phoneNumber));
+	contacts[index].setPhoneNumber(phoneNumber);
+	contacts[index].setDarkestSecret(inputField("darkest secret"));
+	contactCount++;
+}
+
+void PhoneBook::searchContact() {
+	std::cout << std::setw(10) << "Index" << "|";
+	std::cout << std::setw(10) << "First name" << "|";
+	std::cout << std::setw(10) << "Last name" << "|";
+	std::cout << std::setw(10) << "Nickname" << std::endl;
+	for (int i = 0; i < contactCount; i++) {
+		std::cout << std::setw(10) << i + 1 << "|";
+		std::cout << std::setw(10) << formatString(contacts[i].getFirstName()) << "|";
+		std::cout << std::setw(10) << formatString(contacts[i].getLastName()) << "|";
+		std::cout << std::setw(10) << formatString(contacts[i].getNickname()) << std::endl;
+	}
+	std::string input;
+	std::cout << "Enter index of the contact you want to see: ";
+	std::getline(std::cin, input);
+	if (input.empty() || !std::all_of(input.begin(), input.end(), ::isdigit)) {
+        std::cout << "Error: invalid index input" << std::endl;
+        return ;
+    }
+	int index = std::stoi(input) - 1;
+	if (index >= 0 && index < std::min(contactCount, 8)) {
+		std::cout << "First name: " << contacts[index].getFirstName() << std::endl;
+		std::cout << "Last name: " << contacts[index].getLastName() << std::endl;
+		std::cout << "Nickname: " << contacts[index].getNickname() << std::endl;
+		std::cout << "Phone number: " << contacts[index].getPhoneNumber() << std::endl;
+	} else {
+		std::cout << "Index invalid" << std::endl;
+	}
 }
