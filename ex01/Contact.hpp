aff84f45ab61d94fc782cb6d7d90610304c3cb60
Alicja <alicjanamieta@gmail.com>
2024-10-02 20:55:58 +0200

    adding contact header

---
+++ ex01/Contact.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 18:00:33 by anamieta          #+#    #+#             */
+/*   Updated: 2024/10/02 20:17:29 by anamieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <string>
+#include <iostream>
+
+class Contact {
+private:
+	std::string firstName;
+	std::string lastName;
+	std::string nickname;
+	std::string phoneNumber;
+	std::string darkestSecret;
+public:
+	Contact();
+	~Contact();
+	std::string& getFirstName();
+	std::string& getLastName();
+	std::string& getNickname();
+	std::string& getPhoneNumber();
+	void setFirstName(std::string firstName);
+	void setLastName(std::string lastName);
+	void setNickname(std::string nickname);
+	void setPhoneNumber(std::string phoneNumber);
+	void setDarkestSecret(std::string darkestSecret);
+};
+
+#endif
