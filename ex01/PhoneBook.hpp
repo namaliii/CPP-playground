c3ba27320fc0f597b7384b173ca119d374c3504f
Alicja <alicjanamieta@gmail.com>
2024-10-04 18:50:06 +0200

    adding cpp standard flag and linux compilation library

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 18:02:45 by anamieta          #+#    #+#             */
-/*   Updated: 2024/10/04 18:31:15 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/10/04 18:46:30 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "Contact.hpp"
 # include <iomanip>
+// # include <algorithm> for linux compilation
 
 # define MAX_CONTACT 8
 
