6b171f6afe0f3c6859a332db3d535c59bd41dc1c
Alicja <alicjanamieta@gmail.com>
2024-10-21 21:43:36 +0200

    fixing the spaces when several args

--- module 00/ex00/megaphone.cpp
+++ module 00/ex00/megaphone.cpp
@@ -6,11 +6,12 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 20:38:05 by anamieta          #+#    #+#             */
-/*   Updated: 2024/10/20 19:04:08 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/10/21 21:38:51 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char **argv) {
 	if (argc == 1)
@@ -21,6 +22,8 @@ int main(int argc, char **argv) {
 			arg[j] = (toupper(arg[j]));
 		}
 		std::cout << arg;
+		if (argc != i - 1)
+			std::cout << " ";
 	}
 	std::cout << std::endl;
 	return 0;
