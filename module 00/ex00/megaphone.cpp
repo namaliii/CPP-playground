7f2a75ed2765460451acadd3407379a5031da17d
Alicja <alicjanamieta@gmail.com>
2024-10-23 16:38:42 +0200

    zombies are starting eating BraiiiiinnnzzzZ

--- module 00/ex00/megaphone.cpp
+++ module 00/ex00/megaphone.cpp
@@ -6,11 +6,12 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 20:38:05 by anamieta          #+#    #+#             */
-/*   Updated: 2024/10/20 19:04:08 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/10/21 21:48:03 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 int main(int argc, char **argv) {
 	if (argc == 1)
