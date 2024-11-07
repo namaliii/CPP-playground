103e54bf167c4e7be8335f922ab0c0c9f9272eaa
Alicja <alicjanamieta@gmail.com>
2024-11-07 18:32:53 +0100

    getting rid of redundant includes

--- module 03/ex01/ScavTrap.hpp
+++ module 03/ex01/ScavTrap.hpp
@@ -6,14 +6,13 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 16:16:43 by anamieta          #+#    #+#             */
-/*   Updated: 2024/11/07 17:12:58 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/11/07 18:31:50 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
 # define SCAVTRAP_HPP
 
-# include <iostream>
 # include "ClapTrap.hpp"
 
 class ScavTrap : public ClapTrap {
