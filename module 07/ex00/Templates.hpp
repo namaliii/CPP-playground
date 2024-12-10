bf4a7935e24670a8341684fef7d16f4fc4ab607a
Alicja <alicjanamieta@gmail.com>
2024-12-10 18:17:31 +0100

    learning to use templates

---
+++ module 07/ex00/Templates.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   templates.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 18:09:59 by anamieta          #+#    #+#             */
+/*   Updated: 2024/12/10 18:15:36 by anamieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TEMPLATES_HPP
+#define TEMPLATES_HPP
+
+
+#include <iostream>
+#include <string>
+
+template <typename T>
+void swap(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T min(const T& a, const T& b) {
+	if (a < b)
+		return a;
+	return b;
+}
+
+template <typename T>
+T max(const T& a, const T& b) {
+	if (a > b)
+		return a;
+	return b;
+}
+
+#endif
