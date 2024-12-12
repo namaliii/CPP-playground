cda26bc15f7631cd9e1c09ee55fb711ef6f4cff1
Alicja <alicjanamieta@gmail.com>
2024-12-12 22:11:08 +0100

    fixinh template type for function + adding test files

--- module 07/ex01/Iter.hpp
+++ module 07/ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 18:20:48 by anamieta          #+#    #+#             */
-/*   Updated: 2024/12/10 21:36:38 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/12/12 22:07:48 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@
 #define BLUE(text) "\033[38;5;32m" text "\033[0m"
 #define YELLOW(text) "\033[38;5;220m" text "\033[0m"
 
-template <typename T>
-void iter(T* array, size_t length, void (*func)(T&)) {
+template <typename T, typename ft>
+void iter(T* array, size_t length, ft func) {
 	for (size_t i = 0; i < length; i++)
 		func(array[i]);
 }
