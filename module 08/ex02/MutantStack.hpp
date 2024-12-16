4bb4c342baafc7fac7ea5bccda21874029ffc188
Alicja <alicjanamieta@gmail.com>
2024-12-16 23:27:23 +0100

    getting rid of unneccessary functions

--- module 08/ex02/MutantStack.hpp
+++ module 08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 22:02:20 by anamieta          #+#    #+#             */
-/*   Updated: 2024/12/14 23:09:56 by anamieta         ###   ########.fr       */
+/*   Updated: 2024/12/16 23:26:58 by anamieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,6 @@ template<typename T>
 class MutantStack : public std::stack<T> {
 	public:
 		typedef typename std::stack<T>::container_type::iterator iterator;
-		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
 		MutantStack() : std::stack<T>() {}
 		MutantStack(const MutantStack<T>& src) : std::stack<T>(src) {}
@@ -43,15 +40,6 @@ class MutantStack : public std::stack<T> {
 
 		iterator begin() { return std::stack<T>::c.begin(); }
 		iterator end() { return std::stack<T>::c.end(); }
-
-		const_iterator begin() const { return std::stack<T>::c.cbegin(); }
-		const_iterator end() const { return std::stack<T>::c.cend(); }
-
-		reverse_iterator rbegin() { return std::stack<T>::c.rbegin(); }
-		reverse_iterator rend() { return std::stack<T>::c.rend(); }
-
-		const_reverse_iterator rbegin() const { return std::stack<T>::c.crbegin(); }
-		const_reverse_iterator rend() const { return std::stack<T>::c.crend(); }
 };
 
 
