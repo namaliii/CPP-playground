46b00bae754604636ebf257ca9cac47c7ca6b736
Alicja <alicjanamieta@gmail.com>
2025-01-14 22:28:33 +0100

    river of containers

---
+++ module 09/ex03new/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 20:51:58 by anamieta          #+#    #+#             */
+/*   Updated: 2025/01/14 22:04:41 by anamieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cout << "Please insert numbers that I can sort! :)" << std::endl;
+        return 1;
+    }
+    try {
+        PmergeMe sorter(argc, argv);
+
+        sorter.sortAndDisplay();
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
