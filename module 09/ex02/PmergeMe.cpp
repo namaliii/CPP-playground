f99883c3a81f16a5081d4059c7d634ba53e0ff7f
Alicja <alicjanamieta@gmail.com>
2025-01-16 17:12:17 +0100

    optimizing sorting performance using std::vector and std::deque

---
+++ module 09/ex02/PmergeMe.cpp
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 17:43:25 by anamieta          #+#    #+#             */
+/*   Updated: 2025/01/14 22:28:17 by anamieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe(int argc, char** argv) {
+	this->vecInput = parseNumbers<std::vector<int>>(argc, argv);
+	this->deqInput = parseNumbers<std::deque<int>>(argc, argv);
+}
+
+PmergeMe::PmergeMe(const PmergeMe& other)
+	: deqPairs(other.deqPairs),
+		deqMain(other.deqMain),
+		deqAppend(other.deqAppend),
+		deqInput(other.deqInput),
+		vecPairs(other.vecPairs),
+		vecMain(other.vecMain),
+		vecAppend(other.vecAppend),
+		vecInput(other.vecInput) {}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other) {
+	if (this != &other) {
+		deqPairs = other.deqPairs;
+		deqMain = other.deqMain;
+		deqAppend = other.deqAppend;
+		deqInput = other.deqInput;
+
+		vecPairs = other.vecPairs;
+		vecMain = other.vecMain;
+		vecAppend = other.vecAppend;
+		vecInput = other.vecInput;
+	}
+	return *this;
+}
+
+PmergeMe::~PmergeMe() {}
+
+void PmergeMe::sortVector() {
+	std::vector<int> jacob;
+
+	if (isSorted(vecInput)) {
+		vecMain = vecInput;
+		return;
+	}
+	for (size_t i = 0; i + 1 < vecInput.size(); i += 2) {
+		std::vector<int> couple = { std::max(vecInput[i], vecInput[i + 1]), std::min(vecInput[i], vecInput[i + 1]) };
+		vecPairs.push_back(couple);
+	}
+	mergeSortFirsts(vecPairs);
+	for (auto& it : vecPairs) {
+		vecMain.push_back(it[0]);
+		vecAppend.push_back(it[1]);
+	}
+	if (vecInput.size() % 2 != 0) {
+		vecAppend.push_back(vecInput.back());
+	}
+	jacob = generateJsthalNumbers<std::vector<int>>(vecAppend.size());
+	doBinaryInsertion(vecMain, vecAppend, jacob);
+}
+
+void PmergeMe::sortDeque() {
+	std::deque<int> jacob;
+
+	if (isSorted(deqInput)) {
+		deqMain = deqInput;
+		return;
+	}
+	for (size_t i = 0; i + 1 < deqInput.size(); i += 2) {
+		std::vector<int> couple = { std::max(deqInput[i], deqInput[i + 1]), std::min(deqInput[i], deqInput[i + 1]) };
+		deqPairs.push_back(couple);
+	}
+	mergeSortFirsts(deqPairs);
+	for (auto& it : deqPairs) {
+		deqMain.push_back(it[0]);
+		deqAppend.push_back(it[1]);
+	}
+	if (deqInput.size() % 2 != 0) {
+		deqAppend.push_back(deqInput.back());
+	}
+	jacob = generateJsthalNumbers<std::deque<int>>(deqAppend.size());
+	doBinaryInsertion(deqMain, deqAppend, jacob);
+}
+
+void PmergeMe::sortAndDisplay() {
+		std::cout << "Before: ";
+		displaySequence(vecInput);
+		auto start = std::chrono::high_resolution_clock::now();
+		sortVector();
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration<double, std::micro>(end - start);
+
+		std::cout << "After: ";
+		displaySequence(vecMain);
+		std::cout << "Time to process a range of " << vecInput.size()
+					<< " elements with std::vector: "
+					<< std::fixed << std::setprecision(3)
+					<< duration.count() << " us" << std::endl;
+
+		start = std::chrono::high_resolution_clock::now();
+		sortDeque();
+		end = std::chrono::high_resolution_clock::now();
+		duration = std::chrono::duration<double, std::micro>(end - start);
+
+		std::cout << "Time to process a range of " << deqInput.size()
+					<< " elements with std::deque: "
+					<< std::fixed << std::setprecision(3)
+					<< duration.count() << " us" << std::endl;
+}
---
+++ module 09/ex02/PmergeMe.hpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamieta <anamieta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 20:52:16 by anamieta          #+#    #+#             */
+/*   Updated: 2025/01/16 17:05:35 by anamieta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <sstream>
+#include <vector>
+#include <deque>
+#include <list>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <iomanip>
+
+#define RED(text) "\033[38;5;160m" << text << "\033[0m"
+
+class PmergeMe {
+	public:
+		PmergeMe(int argc, char** argv);
+		PmergeMe(const PmergeMe& other);
+		PmergeMe& operator=(const PmergeMe& other);
+		~PmergeMe();
+
+		void sortAndDisplay();
+
+	private:
+		std::deque<std::vector<int>> deqPairs;
+		std::deque<int> deqMain;
+		std::deque<int> deqAppend;
+		std::deque<int> deqInput;
+
+		std::vector<std::vector<int>> vecPairs;
+		std::vector<int> vecMain;
+		std::vector<int> vecAppend;
+		std::vector<int> vecInput;
+
+		void sortVector();
+		void sortDeque();
+};
+
+#include "PmergeMe.tpp"
+
+#endif
